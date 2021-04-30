76ef9e0262fc56fc9152ff53efd6f58a188f5982
ashi-mittal-57 <83442487+ashi-mittal-57@users.noreply.github.com>
2021-04-30 12:08:39 +0530

    Update assessment.php

--- assessment.php
+++ assessment.php
@@ -1,22 +1,25 @@
 
 <?php
-if(isset($_POST['submit'])){
-    {  
-    echo 'hhh';die;
-      $n = $_POST['number1'];  
-      $k = $_POST['number2'];  
-     // $n = 5;
-      //$k = 3;
-      $sum = 0;
-      for($i = 1 ; $i <= $n ; $i++){
-
-          $sum += $i;
-          if($sum == $k ){
-              $sum = $sum - 1 ;
-          }
-      }
-
-      echo $sum;
-	}
+//Check form submit
+if(isset($_POST['submit']))
+{
+    // Get value of consecutive number
+    $n = $_POST['number1'];
+    //Get value of unluckey number
+    $k = $_POST['number2'];
+    //Initialize variable name sum
+    $sum = 0;
+    //Iiterate number for sum using for loop
+    for($i = 1 ; $i <= $n ; $i++){
+        $sum += $i;
+        //Condition for check if any sum match with unluckey number
+        if($sum == $k ){
+            //Substarct lowest values of N = 1 from sum
+            $sum = $sum - 1 ;
+        }
+    }
+    //Print value of sum
+    echo $sum;
 }
+//END of If condition
 ?>
